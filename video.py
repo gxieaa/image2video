92c63f602dcb20ad2f81099bf7a53f27fe8cf927
xieguoyang <xieguoyang123@gmail.com>
2017-02-16 13:19:53 +0800

    clean code

--- video.py
+++ video.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import re
 
+#dir_path = 
 dir_path = '/home/yy/image/pcl_image_all/'
 images = []
 for i in range(3,200):
@@ -10,12 +11,7 @@ for i in range(3,200):
     images.append(img_name)
 
 frame = cv2.imread(images[0])
-#cv2.imshow('first frame', frame)
-#cv2.waitKey(0)
 height, width, channels = frame.shape
-#print height
-#print width
-#print channels
 fourcc = cv2.cv.CV_FOURCC(*'mp4v')
 out = cv2.VideoWriter('test.avi', fourcc, 5.0, (width, height))
 for img in images:
